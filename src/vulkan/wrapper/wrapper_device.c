7392334d4cb4debee623ebc7f1a88dbe3b105797
fish4terrisa-MSDSM <flyingfish.msdsm@gmail.com>
2025-10-18 21:50:33 +0800

    import commit "afe195d" from xMeM/mesa

--- src/vulkan/wrapper/wrapper_device.c
+++ src/vulkan/wrapper/wrapper_device.c
@@ -146,16 +146,9 @@ wrapper_CreateDevice(VkPhysicalDevice physicalDevice,
    if (!device)
       return vk_error(physical_device, VK_ERROR_OUT_OF_HOST_MEMORY);
 
-   list_inithead(&device->command_buffers);
+   list_inithead(&device->command_buffer_list);
+   list_inithead(&device->memory_data_list);
    device->physical = physical_device;
-   device->memorys = _mesa_hash_table_create(NULL,
-                                             _mesa_hash_pointer,
-                                             _mesa_key_pointer_equal);
-   if (!device->memorys) {
-      vk_free2(&physical_device->instance->vk.alloc, pAllocator,
-               device);
-      return vk_error(physical_device, VK_ERROR_OUT_OF_HOST_MEMORY);
-   }
 
    vk_device_dispatch_table_from_entrypoints(
       &dispatch_table, &wrapper_device_entrypoints, true);
@@ -344,7 +337,7 @@ wrapper_command_buffer_create(struct wrapper_device *device,
    wcb->device = device;
    wcb->pool = pool;
    wcb->dispatch_handle = dispatch_handle;
-   list_add(&wcb->link, &device->command_buffers);
+   list_add(&wrapper_command_buffer->link, &device->command_buffer_list);
 
    *pCommandBuffers = wrapper_command_buffer_to_handle(wcb);
 
@@ -426,7 +419,7 @@ wrapper_DestroyCommandPool(VkDevice _device, VkCommandPool commandPool,
 {
    VK_FROM_HANDLE(wrapper_device, device, _device);
    list_for_each_entry_safe(struct wrapper_command_buffer, wcb,
-                            &device->command_buffers, link) {
+                            &device->command_buffer_list, link) {
       if (wcb->pool == commandPool) {
          wrapper_command_buffer_destroy(device, wcb);
       }
@@ -440,7 +433,7 @@ wrapper_DestroyDevice(VkDevice _device, const VkAllocationCallbacks* pAllocator)
 {
    VK_FROM_HANDLE(wrapper_device, device, _device);
    list_for_each_entry_safe(struct wrapper_command_buffer, wcb,
-                            &device->command_buffers, link) {
+                            &device->command_buffer_list, link) {
       wrapper_command_buffer_destroy(device, wcb);
    }
    list_for_each_entry_safe(struct vk_queue, queue, &device->vk.queues, link) {
