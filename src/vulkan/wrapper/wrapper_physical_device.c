78af7622ebdd61ab0be81edaa9b44a48eb36a85c
fish4terrisa-MSDSM <flyingfish.msdsm@gmail.com>
2025-10-18 20:27:18 +0800

    import commit "578ceaf21" from xMeM/mesa

--- src/vulkan/wrapper/wrapper_physical_device.c
+++ src/vulkan/wrapper/wrapper_physical_device.c
@@ -129,6 +129,16 @@ VkResult enumerate_physical_device(struct vk_instance *_instance)
       pdevice->dispatch_table.GetPhysicalDeviceMemoryProperties(
          pdevice->dispatch_handle, &pdevice->memory_properties);
 
+      const char *app_name = instance->vk.app_info.app_name
+         ? instance->vk.app_info.app_name : "wrapper";
+
+      if (pdevice->driver_properties.driverID == VK_DRIVER_ID_QUALCOMM_PROPRIETARY &&
+          pdevice->properties2.properties.driverVersion > VK_MAKE_VERSION(512, 744, 0) &&
+          strstr(app_name, "clvk")) {
+         /* HACK: Fixed clvk not working on qualcomm proprietary driver. */
+         supported_features->globalPriorityQuery = false;
+      }
+
       list_addtail(&pdevice->vk.link, &_instance->physical_devices.list);
    }
 
