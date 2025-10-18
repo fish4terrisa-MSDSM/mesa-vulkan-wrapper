2b68ac7d9cbf3d31785a73303aed8fb13a57e415
fish4terrisa-MSDSM <flyingfish.msdsm@gmail.com>
2025-10-18 20:51:21 +0800

    fix the imported commit "578ceaf"

--- src/vulkan/wrapper/wrapper_physical_device.c
+++ src/vulkan/wrapper/wrapper_physical_device.c
@@ -132,9 +132,7 @@ VkResult enumerate_physical_device(struct vk_instance *_instance)
       const char *app_name = instance->vk.app_info.app_name
          ? instance->vk.app_info.app_name : "wrapper";
 
-      if (pdevice->driver_properties.driverID == VK_DRIVER_ID_QUALCOMM_PROPRIETARY &&
-          pdevice->properties2.properties.driverVersion > VK_MAKE_VERSION(512, 744, 0) &&
-          strstr(app_name, "clvk")) {
+      if (strstr(app_name, "clvk")) {
          /* HACK: Fixed clvk not working on qualcomm proprietary driver. */
          supported_features->globalPriorityQuery = false;
       }
